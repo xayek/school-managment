1bb734918351f6ba33e670eadc2dc741c22b5f7c
xayek <xayekk@gmail.com>
2025-08-08 16:27:24 +0300

    Add Exam resource with CRUD pages and form setup for exam management

---
+++ app/Filament/Resources/ExamResource.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ExamResource\Pages;
+use App\Filament\Resources\ExamResource\RelationManagers;
+use App\Models\Exam;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ExamResource extends Resource
+{
+    protected static ?string $model = Exam::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')->required()->maxLength(255),
+                Forms\Components\DatePicker::make('date')->required(),
+                Forms\Components\Select::make('subject_id')->relationship('subject', 'name')->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('serial_no')->label('No.')->rowIndex(),
+                TextColumn::make('name')->searchable()->sortable(),
+                TextColumn::make('date')->dateTime()->sortable(),
+                TextColumn::make('subject.name')->label('Subject')->searchable()->sortable(),
+                TextColumn::make('created_at')->dateTime()->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListExams::route('/'),
+            'create' => Pages\CreateExam::route('/create'),
+            'edit' => Pages\EditExam::route('/{record}/edit'),
+        ];
+    }
+}
